95b5cb0eeae112ad7ab7dfa86f0e96ef4a9c4019
Niall McCaffrey <innovine1@gmail.com>
2016-04-03 14:33:20 +0000

    Added basic webserver

---
+++ webserver.js
@@ -0,0 +1,7 @@
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello world");
+});
+server.listen(80);
+console.log("Server running");
